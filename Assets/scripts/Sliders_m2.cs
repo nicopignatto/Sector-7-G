479c428e73e85e78181048b98a1550b77642c0f8
Krows-MJ <lukakrows@gmail.com>
2022-11-20 09:05:38 -0400

    creando rama

---
+++ Assets/scripts/Sliders_m2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sliders_m2 : MonoBehaviour
+{
+    [SerializeField] private Slider sld;
+    [SerializeField]private GameObject guja;
+    //public float valor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //valor=0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //guja.transform.eulerAngles= new Vector3(0,0,valor);   
+    }
+
+    public void MovSliders(float value){
+        guja.transform.eulerAngles= new Vector3(0,0,-value*2);
+        guja.transform.eulerAngles= new Vector3(0,0,-sld.value*2); 
+    }
+}
