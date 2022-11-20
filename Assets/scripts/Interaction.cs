a523b3e24fe9906a7aa023502896360b6022255b
nicolas <nicolasvazquezzzz@gmail.com>
2022-11-20 00:52:10 -0300

    imagen

---
+++ Assets/scripts/Interaction.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Interaction : MonoBehaviour
+{
+ 
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MandarMsj()
+    {
+        Debug.Log("esto funca");
+    }
+
+
+}
