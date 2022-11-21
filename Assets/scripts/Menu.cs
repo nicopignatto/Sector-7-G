e5bcb6e7294f025025373a1d78bd867296444730
Krows-MJ <lukakrows@gmail.com>
2022-11-21 05:35:14 -0400

    Menu v2

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject panelCreditos;
+    [SerializeField] private GameObject UIHuman;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +15,12 @@ public class Menu : MonoBehaviour
     }
 
     // Update is called once per frame
-    //void Update(){}
+    void Update()
+    {
+        Vector3 mouspos= Input.mousePosition;
+        if(mouspos.x >= 854f) UIHuman.transform.localScale= new Vector3(-1.8f, 1.8f, 1f);
+        if(mouspos.x <= 750f) UIHuman.transform.localScale= new Vector3(1.8f, 1.8f, 1f);
+    }
 
     public void onPanelCreditos(){
         panelCreditos.SetActive(true);
